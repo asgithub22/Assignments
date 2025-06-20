bf428d19560d938ee149ed6465edf818fc6b21f8
Akash Shrivastava <akash.doe@gmail.com>
2025-06-20 13:03:56 +0530

    removeDuplicateCharacter

--- src/stringOperations/removeDuplicateCharacter.java
+++ src/stringOperations/removeDuplicateCharacter.java
@@ -3,11 +3,12 @@ package stringOperations;
 public class removeDuplicateCharacter {
 
 	public static void main(String[] args) {
-		String input = "Sasarammchanndara";
+		String input = "Akash";
+		String input1 = input.toLowerCase();
 		String result = "";
 
-		for (int i = 0; i < input.length(); i++) { // loop start from ith position to till length of string
-			char ch = input.charAt(i); // extract the character one by one
+		for (int i = 0; i < input1.length(); i++) { // loop start from ith position to till length of string
+			char ch = input1.charAt(i); // extract the character one by one
 
 			// Add character only if it's not already in the result
 			if (!result.contains(String.valueOf(ch))) {
@@ -15,7 +16,7 @@ public class removeDuplicateCharacter {
 			}
 		}
 
-		System.out.println("Original: " + input); // print original string
+		System.out.println("Original: " + input1); // print original string
 		System.out.println("Without duplicates: " + result); // print duplicate removed string
 
 	}
